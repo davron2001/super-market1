192f5ca03a2fffd31f4a15271d44f02b98c1746d
Ages <agessulaymonov070@gmail.c>
2021-06-14 23:31:56 +0500

    Qushildi

---
+++ src/main/java/uz/ages/market/controller/ApiController.java
@@ -0,0 +1,19 @@
+package uz.ages.market.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/hello")
+    public String method() {
+        return "Hello world";
+    }
+
+    @GetMapping("/git")
+    public String git() {
+        return "Hello git";
+    }
+
+}
